eaa67afd785bfcff464540381cc254fde0a86b0e
mayor <mayor@m-mali204-pc10>
2017-10-04 20:02:29 -0300

    04.10.17

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo "<h3>Arquitectura Web</h3>"
+            echo "<h3>Arquitectura Web</h3>";
         ?>
     </body>
 </html>
